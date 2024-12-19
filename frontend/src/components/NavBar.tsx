911ce2a1d2eb3db81d82d4f27e174c80d31988d3
이민선 <woooooooow22@gmail.com>
2024-12-19 22:36:30 +0900

    style: header에 z-index 적용용

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 export const Navbar = () => {
 	return (
-		<header className="h-16 w-screen flex justify-evenly items-center bg-white text-black font-bold fixed">
+		<header className="z-50 h-16 w-screen flex justify-evenly items-center bg-white text-black font-bold fixed">
 			<div>Frontend_Portfolio</div>
 			<ul className="flex gap-x-10">
 				<li>Skills</li>
