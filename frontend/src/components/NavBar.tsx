bccb57cdde4afb709234ba127eef93e2acd7dc87
이민선 <woooooooow22@gmail.com>
2024-12-20 23:50:56 +0900

    feat: header 로고와 메뉴 간간 간격 조정

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
 	return (
 		<header className="z-50 h-16 w-screen flex justify-evenly items-center bg-white text-black font-bold fixed">
-			<div className="w-screen tablet:w-[1000px] laptop:w-screen flex justify-evenly items-center">
+			<div className="w-screen tablet:max-w-[1200px] flex justify-around items-center">
 				<div className="pl-12 tablet:pl-0">Frontend_Portfolio</div>
 				<ul className="flex gap-x-4 invisible tablet:visible">
 					<li>Skills</li>
