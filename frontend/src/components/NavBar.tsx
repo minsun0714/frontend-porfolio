1e7291ff1e55a6eeb666daea47c5031984c5e256
이민선 <woooooooow22@gmail.com>
2024-12-20 19:30:00 +0900

    feat: nav bar 반응형형

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 export const Navbar = () => {
 	return (
 		<header className="z-50 h-16 w-screen flex justify-evenly items-center bg-white text-black font-bold fixed">
-			<div>Frontend_Portfolio</div>
-			<ul className="flex gap-x-10">
-				<li>Skills</li>
-				<li>Projects</li>
-				<li>Education</li>
-				<li>Others</li>
-			</ul>
+			<div className="w-screen p-4 laptop:w-[1024px] flex justify-evenly items-center fixed">
+				<div>Frontend_Portfolio</div>
+				<ul className="flex gap-x-10 invisible laptop:visible">
+					<li>Skills</li>
+					<li>Projects</li>
+					<li>Education</li>
+					<li>Others</li>
+				</ul>
+			</div>
 		</header>
 	);
 };
