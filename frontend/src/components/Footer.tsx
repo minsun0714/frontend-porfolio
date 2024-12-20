4c682d8c8268af221c354da246a7a513b79c615d
이민선 <woooooooow22@gmail.com>
2024-12-20 17:28:43 +0900

    feat: footer 생성성

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+	return (
+		<footer className="h-16 w-screen flex justify-evenly items-center bg-black text-white">
+			<p>© 2024.Lee Minsun. All rights Reserved</p>
+		</footer>
+	);
+};
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { Main } from "@/components/Main/Main";
-import { SkillsView } from "@/components/Skill/SkillsView";
-import { ProjectsView } from "@/components/Projects/ProjectsView";
 import { EducationView } from "@/components/Education/EducationView";
+import { Footer } from "@/components/Footer";
+import { Main } from "@/components/Main/Main";
+import { ProjectsView } from "@/components/Projects/ProjectsView";
+import { SkillsView } from "@/components/Skill/SkillsView";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function Home() {
 			<SkillsView />
 			<ProjectsView />
 			<EducationView />
+			<Footer />
 		</div>
 	);
 }
