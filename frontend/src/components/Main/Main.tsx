cfa32cc9323c498e7eb7211763a576d119e2012b
이민선 <woooooooow22@gmail.com>
2025-01-01 23:34:00 +0900

    feat: 한줄 자기소개 작성성

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -13,7 +13,10 @@ export const Main = () => {
 		},
 	];
 	return (
-		<section id="main-section" className="relative flex justify-center items-center h-screen w-screen">
+		<section
+			id="main-section"
+			className="relative flex justify-center items-center h-screen w-screen"
+		>
 			<Image
 				src="/main/image.png"
 				alt="메인 화면"
@@ -25,7 +28,10 @@ export const Main = () => {
 			<div className="absolute top-1/3 z-10 font-bold text-center w-full">
 				<h1 className="text-gray-400 text-3xl font-bold">프론트엔드 개발자</h1>
 				<h1 className="text-blue-500 text-6xl font-bold py-4">이민선</h1>
-				<p className="text-black font-light">자기소개 한 줄</p>
+				<p className="invisible tablet:visible text-black font-light">
+					도전적인 과제에서 배움을 찾아 성장의 관성을 만드는 프론트엔드 개발자
+					이민선입니다.
+				</p>
 			</div>
 			<ul className="absolute bottom-20 grid grid-cols-1 tablet:grid-cols-2 font-light text-black bg-opacity-40 gap-2 mx-auto tablet:gap-x-20 laptop:gap-x-80">
 				{contactInfo.map((info, idx) => {
